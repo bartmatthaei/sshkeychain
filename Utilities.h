b221963ef7a3eac676ecd1529fea15a6907add30
bart <bart@1a270b93-f1e6-0310-a04b-e8f41df0ee4d>
2004-12-10 20:36:02 +0000

    Removed header

--- Utilities.h
+++ Utilities.h
@@ -1,11 +1,3 @@
-//
-//  Utilities.h
-//  SSHKeychain
-//
-//  Created by Kevin Ballard on 12/8/04.
-//  Copyright 2004 Kevin Ballard. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 NSString *CreateUUID();
--- Utilities.m
+++ Utilities.m
@@ -1,11 +1,3 @@
-//
-//  Utilities.m
-//  SSHKeychain
-//
-//  Created by Kevin Ballard on 12/8/04.
-//  Copyright 2004 Kevin Ballard. All rights reserved.
-//
-
 #import "Utilities.h"
 #import <CoreFoundation/CoreFoundation.h>
 
