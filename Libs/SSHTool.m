3b30ea082971bfa4ce4dd5b53a6bc70d04247bcc
mrowe <mrowe@1a270b93-f1e6-0310-a04b-e8f41df0ee4d>
2005-12-27 18:13:01 +0000

    Primarily cosmetic changes to SSHTool.  One minor functional change is that handleTerminateWithSelector:toObject:withInfo: now retains both theObject and theInfo to prevent them from disappearing out from under it.  They are released when the task termination notification comes in.

--- Libs/SSHTool.m
+++ Libs/SSHTool.m
@@ -9,33 +9,26 @@
 /* Construct a SSHTool object for the required tool. */
 + (id)toolWithName:(NSString *)toolname
 {
-	SSHTool *tool = [[[self alloc] init] autorelease];
-	
-	[tool setPath:[[[NSUserDefaults standardUserDefaults] 
-			stringForKey:SSHToolsPathString] 
-			stringByAppendingPathComponent:toolname]];
-	
-	return tool;
+	return [self toolWithPath:[[[NSUserDefaults standardUserDefaults] 
+					stringForKey:SSHToolsPathString] 
+					stringByAppendingPathComponent:toolname]];
 }
 
 /* Construct a SSHTool object for the required path. */
 + (id)toolWithPath:(NSString *)path
 {
 	SSHTool *tool = [[[self alloc] init] autorelease];
-	
 	[tool setPath:path];
-	
 	return tool;
 }
 
 - (id)init
 {
-	if(self = [super init])
-	{
-		task = [[NSTask alloc] init];
-		[task setStandardInput:[NSFileHandle fileHandleWithNullDevice]];
-		toolPath = nil;
-	}
+	if (! (self = [super init]))
+		return nil;
+		
+	task = [[NSTask alloc] init];
+	[task setStandardInput:[NSFileHandle fileHandleWithNullDevice]];
 
 	return self;
 }
@@ -46,6 +39,8 @@
 	
 	[task release];
 	[toolPath release];
+	[terminateObject release];
+	[terminateInfo release];
 	
 	[super dealloc];
 }
@@ -53,8 +48,9 @@
 /* Set the path. */
 - (void)setPath:(NSString *)path
 {
-	[toolPath autorelease];
+	NSString *oldPath = toolPath;
 	toolPath = [path copy];
+	[oldPath release];
 }
 
 /* Set the argument for the NSTask. */
@@ -78,66 +74,46 @@
 /* Add environment variable. */
 - (void)setEnvironmentVariable:(NSString *)variable withValue:(NSString *)value
 {
-	NSMutableDictionary *env = [NSMutableDictionary dictionaryWithDictionary:[task environment]];
+	if (!variable || !value)
+		return;
 
-	if((variable) && (value))
-	{
-		[env setObject:value forKey:variable];
-		[task setEnvironment:env];
-	}
+	NSMutableDictionary *env = [[task environment] mutableCopy];
+	[env setObject:value forKey:variable];
+	[task setEnvironment:env];
+	[env release];
 }
 
 /* Launch and return the stdout as an NSString. */
 - (NSString *)launchForStandardOutput
 {
 	NSPipe *thePipe = [[[NSPipe alloc] init] autorelease];
-	NSString *theOutput;
 
 	[task setStandardOutput:thePipe];
 
-	if([self launchAndWait] == NO)
-	{
+	if (![self launchAndWait])
 		return nil;
-	}
-
-	/* Retrieve the stdout as a NSPipe. */
-	thePipe = [task standardOutput];
 
 	/* Put the data from thePipe to theOutput. */
-	theOutput = [[[NSString alloc] initWithData:[[thePipe fileHandleForReading] readDataToEndOfFile] encoding:NSASCIIStringEncoding] autorelease];
-
-	return theOutput;
+	NSData *theOutput = [[thePipe fileHandleForReading] readDataToEndOfFile];
+	return [[[NSString alloc] initWithData:theOutput encoding:NSUTF8StringEncoding] autorelease];
 }
 
 /* Launch and wait until exit, returning a BOOL to report success or failure. */
 - (BOOL)launchAndWait
 {
-	if(![self launch])
-	{
+	if (![self launch])
 		return NO;
-	}
 	
 	[task waitUntilExit];
-	
-	if([task terminationStatus] == 0)
-	{
-		return YES;
-	}
-	
-	else
-	{
-		return NO;
-	}
+	return ![task terminationStatus];
 }
 
 /* Launch. */
 - (BOOL)launch
 {
 	/* Let's see if the path is accessible. */
-	if(![[NSFileManager defaultManager] isExecutableFileAtPath:toolPath])
-	{
+	if (![[NSFileManager defaultManager] isExecutableFileAtPath:toolPath])
 		return NO;
-	}
 
 	/* Set the launchpath to the path instance variable. */
 	[task setLaunchPath:toolPath];
@@ -146,15 +122,11 @@
 	NS_DURING
 		[task launch];
 	NS_HANDLER
-		if([[localException name] isEqualToString:NSInvalidArgumentException])
-		{
+		if ([[localException name] isEqualToString:NSInvalidArgumentException])
 			retValue = NO;
-		}
 		
 		else
-		{
 			[localException raise];
-		}
 	NS_ENDHANDLER
 	
 	return retValue;
@@ -169,24 +141,18 @@
 /* Handle terminate notifications. */
 - (void)handleTerminateWithSelector:(SEL)theSelector toObject:(id)theObject withInfo:(id)theInfo
 {
-	if((!theSelector) || (!theObject)) 
-	{
+	if (!theSelector || !theObject) 
 		return;
-	}
-	
+
 	terminateSelector = theSelector;
-	terminateObject = theObject;
+	terminateObject = [theObject retain];
 	
-	if(theInfo)
-	{
+	if (theInfo)
 		terminateInfo = theInfo;
-	}
-	
 	else
-	{
 		terminateInfo = self;
-	}
-	
+
+	[terminateInfo retain];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(terminatedTaskNotification:)
 						name:NSTaskDidTerminateNotification object:nil];
 }
@@ -194,9 +160,13 @@
 /* Callback for terminated NSTasks. */
 - (void)terminatedTaskNotification:(NSNotification *)notification
 {
-	if([notification object] == task)
+	if ([notification object] == task)
 	{
 		[terminateObject performSelector:terminateSelector withObject:terminateInfo];
+		[terminateObject release];
+		[terminateInfo release];
+		terminateObject = nil;
+		terminateInfo = nil;
 	}
 }
  
